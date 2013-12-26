f0b5537cece62a35b50a8b13b016758dca29a30e
godyZ <godyZhang@gmail.com>
2013-12-26 16:00:36 +0800

    Initial Commit

---
+++ HFCampus/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HFCampus
+//
+//  Created by zhangrongjian on 13-12-26.
+//  Copyright (c) 2013年 zgy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ HFCampus/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HFCampus
+//
+//  Created by zhangrongjian on 13-12-26.
+//  Copyright (c) 2013年 zgy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
